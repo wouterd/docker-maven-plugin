9c303c211b064ad8691455fb769e5a8261e21ff7
Wouter Danes <wouter.danes@gmail.com>
2014-07-23 01:01:11 +0200

    VerifyMojo now logs which goals have caused errors.

--- src/main/java/net/wouterdanes/docker/maven/VerifyMojo.java
+++ src/main/java/net/wouterdanes/docker/maven/VerifyMojo.java
@@ -1,5 +1,9 @@
 package net.wouterdanes.docker.maven;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.InstantiationStrategy;
@@ -16,7 +20,19 @@ public class VerifyMojo extends AbstractDockerMojo {
     @Override
     protected void doExecute() throws MojoExecutionException, MojoFailureException {
 
-        if (!getPluginErrors().isEmpty()) {
+        List<DockerPluginError> errors = getPluginErrors();
+        if (!errors.isEmpty()) {
+
+            Set<String> erronousGoals = new HashSet<>();
+            for (DockerPluginError error : errors) {
+                erronousGoals.add(error.getGoal());
+            }
+            StringBuilder sb = new StringBuilder("The following goals had errors: \n");
+            for (String goal : erronousGoals) {
+                sb.append(" - ").append(goal).append('\n');
+            }
+            getLog().error(sb);
+
             throw new MojoFailureException("Errors occurred, stopping the build");
         }
 
--- src/test/java/net/wouterdanes/docker/maven/VerifyMojoTest.java
+++ src/test/java/net/wouterdanes/docker/maven/VerifyMojoTest.java
@@ -5,8 +5,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
 
 public class VerifyMojoTest {
 
@@ -23,13 +26,16 @@ public class VerifyMojoTest {
     }
 
     @Test(expected = MojoFailureException.class)
-    public void testThatBuildFailsWhenErrorsHaveHappened() throws Exception {
+    public void testThatBuildFailsAndLogsAnErrorWhenErrorsHaveHappened() throws Exception {
 
         DockerPluginError error = new DockerPluginError("some-goal", "Something went wrong", null);
         fakePluginContext.put("errors", Collections.singletonList(error));
+        Log fakeLog = Mockito.mock(Log.class);
+        mojo.setLog(fakeLog);
 
         mojo.execute();
 
+        Mockito.verify(fakeLog, Mockito.atLeastOnce()).error(Matchers.anyString());
     }
 
     @Test
