530526fa8fca8196b0263e9b7721151ff2a610b0
Mats Svefors <mats@info.nl>
2014-10-31 16:10:59 +0100

    no gzip

--- src/main/java/net/wouterdanes/docker/provider/RemoteApiBasedDockerProvider.java
+++ src/main/java/net/wouterdanes/docker/provider/RemoteApiBasedDockerProvider.java
@@ -190,8 +190,8 @@ public abstract class RemoteApiBasedDockerProvider implements DockerProvider {
     private static byte[] getTgzArchiveForFiles(final ImageBuildConfiguration image) {
 	    ByteArrayOutputStream baos = new ByteArrayOutputStream();
 	    try (
-			    CompressorOutputStream gzipStream = new CompressorStreamFactory().createCompressorOutputStream("gz", baos);
-			    ArchiveOutputStream tar = new ArchiveStreamFactory().createArchiveOutputStream("tar", gzipStream)
+
+			    ArchiveOutputStream tar = new ArchiveStreamFactory().createArchiveOutputStream("tar", baos)
 	    ) {
 		    addToTar(tar, image.getDockerFile(), "Dockerfile");
 
@@ -202,8 +202,8 @@ public abstract class RemoteApiBasedDockerProvider implements DockerProvider {
 		    }
 
 		    tar.flush();
-		    gzipStream.flush();
-	    } catch (CompressorException | ArchiveException | IOException e) {
+		    baos.flush();
+	    } catch (ArchiveException | IOException e) {
 		    throw new IllegalStateException("Unable to create output archive", e);
 	    }
 	    return baos.toByteArray();
